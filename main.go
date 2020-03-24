72d009f6c3bae8c2ee81bc4e99a3cbd2aeccf124
Tomasz Osinski <osinstom@gmail.com>
2020-03-24 18:57:15 +0100

    WIP: Need to implement support for PPP

--- main.go
+++ main.go
@@ -6,18 +6,30 @@ import (
 	"github.com/intel-go/nff-go/packet"
 	"github.com/intel-go/nff-go/types"
 	"github.com/intel-go/nff-go/common"
+	"errors"
+	"bytes"
+	"encoding/binary"
 )
 
 var pkt_cnt int = 0
 
 var sessionManager SessionManager
 
-var f *flow.Flow
+var codeSessionEventMap = map[uint32]SessionEvent {
+	packet.PADI 			: PADI,
+	packet.PADO 			: PADO,
+	packet.PADR 			: PADR,
+	packet.PADS 			: PADS,
+	(packet.LCP<<8)|0x01 	: LCP_ConfReq,
+}
 
 func main() {
 	fmt.Println("App started.")
 	sessionManager = SessionManager{SendReplyCallback: send}
+
 	config := flow.Config{
+		NeedKNI:  true,
+		DPDKArgs: []string{"--no-pci", "--vdev=eth_af_packet0,iface=" + "eth0"},
 		DisableScheduler: false,
 		BurstSize: 1,
 	}
@@ -28,39 +40,71 @@ func main() {
 	}
 	//inQueue := 1
 
-	port := "eth0" // TODO: hardcoded
-	firstFlow, err := flow.SetReceiverOS(port)
+	firstFlow, err := flow.SetReceiver(0)
 	//firstFlow, err := flow.SetReceiverXDP(port, inQueue)
 	flow.CheckFatal(flow.SetHandler(firstFlow, dumper, nil))
 	flow.CheckFatal(flow.SetHandler(firstFlow, handleVXLAN, nil))
 	flow.CheckFatal(flow.SetHandlerDrop(firstFlow, handlePPPoE, nil))
-	flow.CheckFatal(flow.SetSenderOS(firstFlow, port))
-	f = firstFlow
+	flow.CheckFatal(flow.SetSender(firstFlow, 0))
 	flow.CheckFatal(flow.SystemStart())
 }
 
-func send() {
+func send(ctx SessionContext) {
 	fmt.Println("Callback invoked!")
 	pkt, err := packet.NewPacket()
 	if err != nil {
 		common.LogFatal(common.Debug, err)
 	}
 
-	// FIXME: hardcoded addresses
-	pkt.Ether.DAddr = types.MACAddress{0x00, 0x07, 0x0d, 0xaf, 0xf4, 0x54}
-	pkt.Ether.SAddr = types.MACAddress{0x00, 0x07, 0x0d, 0xaf, 0xf4, 0x55}
-	pkt.Ether.EtherType = types.SwapPPPoEDNumber
-	pkt.ParseL3()
+	var totalLen uint16
+	tags := getTagsFromAttributes(ctx.attributes, &totalLen)
+
+	if packet.InitEmptyPPPoEDPacket(pkt, uint(totalLen)) == false {
+		fmt.Println("Cannot initalizie PPPoED packet!")
+		return
+	}
+
+	pkt.Ether.DAddr = ctx.subscriberMac
+	pkt.Ether.SAddr = flow.GetPortMACAddress(0)
+
 	pppoe := pkt.GetPPPoEDNoTags()
-	pppoe.SessionId = 0x0001
-	pppoe.Code = 0x09
+	pppoe.VersionType = 0x11
+	pppoe.SessionId = ctx.sessionId
+	pppoe.Len = packet.SwapBytesUint16(totalLen)
+	pppoe.Tags = tags
 
+	code, err := fromSessionEventToPPPoECode(ctx.event)
+	if err != nil {
+		common.LogDebug(common.Debug, "Sending reply failed: ", err)
+		return
+	}
+	pppoe.Code = code
 
-	pkt.SendPacketOS(flow.GetIODevice("eth0").(int))
-	//socketID := flow.GetIODevice("eth0")
-	//fmt.Println("Success? ", ok)
-	//pkt.SendPacket(0)
+	pkt.PacketBytesChange(types.EtherLen + types.PPPoELen, convertToBytes(pppoe.Tags))
 
+	fmt.Println("Sending packet: ", pkt.GetRawPacketBytes())
+	pkt.SendPacket(0)
+}
+
+func convertToBytes(tags []packet.PPPoETag) []byte {
+	var bin_buf bytes.Buffer
+	for _, tag := range tags {
+		binary.Write(&bin_buf, binary.BigEndian, tag.Type)
+		binary.Write(&bin_buf, binary.BigEndian, tag.Len)
+		bin_buf.Write(tag.Value)
+	}
+	return bin_buf.Bytes()
+}
+
+func getTagsFromAttributes(attrs map[string]string, totalLen *uint16) []packet.PPPoETag {
+	var tags []packet.PPPoETag
+	for key, val := range attrs {
+		tags = append(tags, packet.PPPoETag{Type: packet.PPPoETagMapReversed[key],
+										    Len: uint16(len([]byte(val))),
+											Value: []byte(val)})
+		*totalLen += 4 + uint16(len([]byte(val)))
+	}
+	return tags
 }
 
 func dumper(currentPacket *packet.Packet, context flow.UserContext) {
@@ -93,21 +137,46 @@ func handlePPPoE(current *packet.Packet, ctx flow.UserContext) bool {
 	if current.Ether.EtherType == types.SwapPPPoEDNumber {
 		p := current.GetPPPoED()
 		if p != nil {
-			fmt.Println("Got PPPoED packet ", p.Code, p.Len)
+			fmt.Println("Got PPPoED packet ", p.Code, packet.SwapBytesUint16(p.Len))
 		}
-		fmt.Println(p.Tags)
+		fmt.Println("Tags: ", p.Tags)
 		sessionCtx.sessionId = p.SessionId
-		//sessionCtx.attributes = p.Tags
+		sessionCtx.event = fromPPPCodeToSessionEvent(0, p.Code)
 	} else if current.Ether.EtherType == types.SwapPPPoESNumber {
-		current.GetPPPoES()
+		p := current.GetPPPoES()
+		sessionCtx.sessionId = p.SessionId
+		fmt.Println("PPPoES packet: ", packet.SwapBytesUint16(p.Protocol), p.Code)
+
+
+
+
+		sessionCtx.event = fromPPPCodeToSessionEvent(packet.SwapBytesUint16(p.Protocol), p.Code)
 	} else {
 		// Should drop
 		return false
 	}
+	sessionCtx.subscriberMac = current.Ether.SAddr
 	// handle session event in separate goroutine
 	go sessionManager.HandleSessionEvent(sessionCtx)
 
 	return false
+}
+
+func fromPPPCodeToSessionEvent(protocol uint16, code uint8) SessionEvent {
+	event, ok := codeSessionEventMap[uint32(protocol)<<8|uint32(code)]
+	if ok {
+		return event
+	}
+	return UNKNOWN
+}
+
+func fromSessionEventToPPPoECode(evt SessionEvent) (uint8, error) {
+	for key, val := range codeSessionEventMap {
+		if val == evt {
+			return uint8(key), nil
+		}
+	}
+	return 0, errors.New("PPP Code for SessionEvent doesn't exist")
 }
 
 //func handleSession(ctx SessionContext) {
--- session_mgr.go
+++ session_mgr.go
@@ -4,14 +4,21 @@ import (
 	"fmt"
 )
 
+type SessionEvent uint16
+
+// List of Session Events
 const (
-	PADI = iota
+	UNKNOWN SessionEvent = iota
+	PADI
 	PADR
 	PADO
 	PADS
+	LCP_ConfReq
 )
 
 type SessionContext struct {
+	event           SessionEvent
+	subscriberMac   [6]uint8
 	sessionId 		uint16
 	attributes 		map[string]string
 }
@@ -21,10 +28,30 @@ type SessionHandler interface {
 }
 
 type SessionManager struct {
-	SendReplyCallback func()
+	SendReplyCallback func(ctx SessionContext)
 }
 
+// This function is just a prototype for the test purpose.
+// It has to be re-implemented in the future.
 func (sm SessionManager) HandleSessionEvent(ctx SessionContext) {
-	fmt.Println("Handling session ", ctx.sessionId)
-	sm.SendReplyCallback()
+	fmt.Println("Handling session ", ctx)
+	switch ctx.event {
+		case PADI: {
+			ctx.event = PADO
+		}
+		case PADR: {
+			ctx.event = PADS
+			ctx.sessionId = 0x1111
+		}
+		case LCP_ConfReq: {
+			fmt.Println("LCP Conf Req")
+		}
+		default: {
+			return
+		}
+	}
+	ctx.attributes = map[string]string{}
+	ctx.attributes["Service-Name"] = ""
+	ctx.attributes["AC-Name"] = "cbr_bng1_re0"
+	sm.SendReplyCallback(ctx)
 }
